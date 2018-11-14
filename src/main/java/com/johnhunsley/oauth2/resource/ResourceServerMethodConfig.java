b3ba61399ab298cc2a58abcc35c0a25fe3dfa4e1
U0181792 <john.hunsley@thomsonreuters.com>
2018-11-14 21:07:46 +0000

    added License

--- src/main/java/com/johnhunsley/oauth2/resource/ResourceServerMethodConfig.java
+++ src/main/java/com/johnhunsley/oauth2/resource/ResourceServerMethodConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
